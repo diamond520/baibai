e1ee5e740521481d24d947cc36390d92f2f13ae6
diamond <diamond.cc@gmail.com>
2017-03-03 09:57:37 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <home>test</home>
+    <!-- <step-1></step-1> -->
+  </div>
+</template>
+
+<script>
+import home from './home.vue';
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      status: null
+    }
+  },
+  components: {home: home}
+}
+</script>
+
+<style lang="scss">
+@import url('./assets/css/style.css');
+</style>
---
+++ src/home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="home_page">
+    <div id="KV"></div>
+    <img src="./assets/images/emperor2.png" alt="" class="emperor3">
+    <img src="./assets/images/item_01.png" alt="" class="pigeon1">
+    <img src="./assets/images/item_02.png" alt="" class="pigeon2">
+    <img src="./assets/images/item_03.png" alt="" class="pigeon3">
+    <img src="./assets/images/item_04.png" alt="" class="cloud">
+    <img src="./assets/images/item_06.png" alt="" class="cloudtree">
+    <img src="./assets/images/item_07.png" alt="" class="cloudman">
+    <img src="./assets/images/item_08.png" alt="" class="balloon1">
+    <img src="./assets/images/item_09.png" alt="" class="balloon2">
+    <img src="./assets/images/tree.png" alt="" class="tree">
+    <img src="./assets/images/cloud_01.png" alt="" class="cloud2">
+    <div id="home" class="main_container">
+      <div class="content">
+        <div class="home_txt_area">
+          <p class="people_count"><span class="count_txt">目前已有</span><span class="number">{{ number }}</span><span class="count_txt">人參加遊戲！</span></p>
+          <img src="./assets/images/txt.png" alt="" class="home_txt">
+          <img src="./assets/images/txt2.png" alt="" class="home_txt_mobile">
+          <a href="stdsadep2.html" class="btn1"><img src="./assets/images/btn1.gif" alt=""></a>
+        </div>
+        
+      </div>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home_page',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      status: null,
+      number: 300
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
